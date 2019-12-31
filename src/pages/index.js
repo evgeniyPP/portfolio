8758ad5c4992dbd2f1d0ed4abe8be384f8d37b1e
evgeniyPP <amenat12@mail.ru>
2019-12-31 13:26:08 +0300

    styled-components changed to css modules & smart-grid added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,124 +6,40 @@ import {
   faVk,
   faTelegramPlane
 } from '@fortawesome/free-brands-svg-icons'
-import styled from 'styled-components'
-import backgroundImage from '../images/bg.png'
-
-const Wrapper = styled.div`
-  min-height: 100vh;
-  background: center / cover no-repeat url(${backgroundImage});
-  color: #fff;
-  display: flex;
-  flex-wrap: wrap;
-`
-const LeftSide = styled.div`
-  flex: 1;
-  align-self: center;
-  margin-left: 3rem;
-
-  h1 {
-    margin-bottom: 1rem;
-  }
-  h2 {
-    font-weight: normal;
-    margin-bottom: 1.5rem;
-  }
-`
-
-const Links = styled.div`
-  a {
-    color: #fff;
-    margin-right: 1rem;
-  }
-`
-const Icon = styled(FontAwesomeIcon)`
-  font-size: 2rem;
-`
-
-const RightSide = styled.div`
-  margin: 2rem 1rem;
-  max-width: 45%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`
-
-const AboutMe = styled.div`
-  h3 {
-  }
-
-  div {
-    display: flex;
-  }
-
-  p {
-  }
-
-  img {
-    display: block;
-    max-width: 100%;
-    align-self: flex-start;
-    margin-left: 1rem;
-    margin-right: 1rem;
-    border-radius: 50%;
-  }
-`
-
-const Navbar = styled.div`
-  width: 70%;
-  margin: 0 auto;
-`
-
-const NavItem = styled(Link)`
-  color: #fff;
-  display: block;
-  text-decoration: none;
-  padding: 1rem 2rem;
-  border: 1px solid #fff;
-  transition: all 0.2s;
-  font-size: 1.5rem;
-  text-align: center;
-  font-weight: bold;
-
-  &:hover {
-    background: #fff;
-    color: #000;
-    transition: all 0.2s;
-  }
-`
+import styles from '../styles/index.module.less'
 
 export default () => (
-  <Wrapper>
-    <LeftSide>
+  <div className={styles.wrapper}>
+    <div className={styles.leftSide}>
       <h1>Евгений Петрянкин</h1>
       <h2>Веб-разработчик</h2>
-      <Links>
+      <div className={styles.links}>
         <a
           href="https://github.com/evgeniyPP"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Icon icon={faGithub} />
+          <FontAwesomeIcon className={styles.icon} icon={faGithub} />
         </a>
         <a
           href="https://vk.com/fkeromanov"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Icon icon={faVk} />
+          <FontAwesomeIcon className={styles.icon} icon={faVk} />
         </a>
         <a
           href="https://t.me/evgeniy_pp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Icon icon={faTelegramPlane} />
+          <FontAwesomeIcon className={styles.icon} icon={faTelegramPlane} />
         </a>
-      </Links>
-    </LeftSide>
+      </div>
+    </div>
 
-    <RightSide>
-      <AboutMe>
+    <div className={styles.rightSide}>
+      <div className={styles.aboutMe}>
         <h3>Обо мне</h3>
         <div>
           <p>
@@ -140,13 +56,19 @@ export default () => (
             alt="my avatar"
           />
         </div>
-      </AboutMe>
+      </div>
 
-      <Navbar>
-        <NavItem to="/skills">Мой стек</NavItem>
-        <NavItem to="/projects">Мои проекты</NavItem>
-        <NavItem to="/contactme">Связаться со мной</NavItem>
-      </Navbar>
-    </RightSide>
-  </Wrapper>
+      <div className={styles.navbar}>
+        <Link className={styles.navItem} to="/skills">
+          Мой стек
+        </Link>
+        <Link className={styles.navItem} to="/projects">
+          Мои проекты
+        </Link>
+        <Link className={styles.navItem} to="/contactme">
+          Связаться со мной
+        </Link>
+      </div>
+    </div>
+  </div>
 )
