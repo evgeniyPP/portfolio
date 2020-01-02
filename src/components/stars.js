cf797ea93b1eae8a2c41fc94ba4fc159ef878907
evgeniyPP <amenat12@mail.ru>
2020-01-02 12:37:43 +0300

    skills page added

---
+++ src/components/stars.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as regStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons'
+import styles from '../styles/stars.module.less'
+
+export default ({ level }) => {
+  const stars = []
+  for (let i = 0; i < level; i++) {
+    stars.push(<FontAwesomeIcon icon={solidStar} className={styles.star} />)
+  }
+  for (let i = 0; i < 10 - level; i++) {
+    stars.push(<FontAwesomeIcon icon={regStar} className={styles.star} />)
+  }
+
+  return <div>{stars}</div>
+}
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,13 +1,65 @@
 import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 import Header from '../components/header'
+import Stars from '../components/stars'
 import styles from '../styles/skills.module.less'
 
 export default () => {
+  const query = useStaticQuery(graphql`
+    query {
+      allContentfulSkills(sort: { fields: createdAt, order: ASC }) {
+        edges {
+          node {
+            technology
+            level
+          }
+        }
+      }
+    }
+  `)
+
+  const skills = query.allContentfulSkills.edges.map(edge => {
+    const { technology, level } = edge.node
+    return (
+      <li key={technology} className={styles.skill}>
+        <h3>{technology}</h3>
+        <div className={styles.level}>
+          <Stars className={styles.stars} level={level} />
+          <span>{level}</span>
+        </div>
+      </li>
+    )
+  })
+
   return (
     <div className="wrapper">
       <div className={styles.content}>
         <Header>Мой стек</Header>
+        <div className={styles.description}>
+          <p>
+            Что я еще люблю в программировании – это возможность постоянно учить
+            что-то новое. Я знаю довольно много различных технологий и умею
+            применять их на практике. Несмотря на то, что я на данный момент
+            больше осведомлен во фронтенде, также интересуюсь бэкенд-частью. До
+            Middle-уровня мне не хватает знания стандартных шаблонов разработки,
+            я могу перемудрить там, где что-то можно сделать "в лоб". У меня нет
+            опыта работы в команде разработчиков.
+          </p>
+          <p>
+            Поэтому я на данный момент оценил бы себя как Upper-junior Front-end
+            разработчика.
+          </p>
+          <p>
+            Но моя краткосрочная цель – устроиться в компанию и за полгода
+            упорной работы стать полноценным членов команды уровня Middle.
+          </p>
+        </div>
+        <ul className={styles.skills}>
+          {skills}
+          <li className={styles.other}>
+            А также: <span>Git, Svelte, GatsbyJS, GraphQL</span> и многое другое
+          </li>
+        </ul>
       </div>
     </div>
   )
